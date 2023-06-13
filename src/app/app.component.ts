e372a48330659859dc0ab932b9b71a89cf31f323
alexlamb10 <99216731+alexlamb10@users.noreply.github.com>
2023-06-13 10:32:38 -0600

    Added Validators and html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { map, startWith } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public form: FormGroup = this._fb.group({
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+  });
+
+  public formUpdates$ = this.form.valueChanges.pipe(
+    startWith(this.form.value),
+    map((values) => {
+      return {
+        ...values,
+        invalid: this.form.invalid,
+      };
+    })
+  );
+
+  constructor(private _fb: FormBuilder) {}
+
+  submit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
